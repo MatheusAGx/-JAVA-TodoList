1b5b58c6d1bf0a744b43417ab08992da4fdc56ea
MatheusAGx <matt.ag@outlook.com>
2024-09-11 10:50:54 -0300

    first commit

---
+++ api-tafefas/src/main/java/br/com/matheusagx/api_tafefas/ApiTafefasApplication.java
@@ -0,0 +1,13 @@
+package br.com.matheusagx.api_tafefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTafefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTafefasApplication.class, args);
+	}
+
+}
---
+++ api-tafefas/src/test/java/br/com/matheusagx/api_tafefas/ApiTafefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matheusagx.api_tafefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTafefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
