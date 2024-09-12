24a3a972cc592a3a95aa001f8dcb2cffb8ec7b48
MatheusAGx <matt.ag@outlook.com>
2024-09-11 21:39:57 -0300

    feat: estrutura do projeto: classes, service e repository

---
+++ api-tafefas/src/main/java/br/com/matheusagx/api_tafefas/repository/TodoRepository.java
@@ -0,0 +1,7 @@
+package br.com.matheusagx.api_tafefas.repository;
+
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+
+public class TodoRepository extends JpaRepositoriesAutoConfiguration{
+    
+}
