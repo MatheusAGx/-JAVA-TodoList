71ad534617f60977bcad3b15fe6a3e66c80deaf6
MatheusAGx <matt.ag@outlook.com>
2024-09-11 22:33:18 -0300

    feat: adicao do controller

--- api-tafefas/src/main/java/br/com/matheusagx/api_tafefas/repository/TodoRepository.java
+++ api-tafefas/src/main/java/br/com/matheusagx/api_tafefas/repository/TodoRepository.java
@@ -1,7 +1,29 @@
 package br.com.matheusagx.api_tafefas.repository;
 
+import java.util.List;
+
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.cdi.JpaRepositoryExtension;
+import br.com.matheusagx.api_tafefas.entity.Todo;
 
 public class TodoRepository extends JpaRepositoriesAutoConfiguration{
+
+
+
+    public List<Todo> findAll(Sort sort) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    public void save(Todo todo) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
     
 }
