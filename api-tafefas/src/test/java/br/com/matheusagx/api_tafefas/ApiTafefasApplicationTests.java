65b6da6460b8e873d72c41ad0b1db9b17218478e
MatheusAGx <matt.ag@outlook.com>
2024-09-15 21:51:44 -0300

    test: criado testes automatizados POST e PUT

--- api-tafefas/src/test/java/br/com/matheusagx/api_tafefas/ApiTafefasApplicationTests.java
+++ api-tafefas/src/test/java/br/com/matheusagx/api_tafefas/ApiTafefasApplicationTests.java
@@ -1,13 +1,79 @@
 package br.com.matheusagx.api_tafefas;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import br.com.matheusagx.api_tafefas.entity.Todo;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ApiTafefasApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo("todo 1", "desc todo 1", false, 1);
+	
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(todo)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$").isArray()
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+			.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+			.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+			.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
 	}
+
+	@Test
+	void testCreateTodoFailure() {
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(
+				new Todo("", "", false, 0)
+			)
+			.exchange()
+			.expectStatus().isBadRequest();
+	}
+
+	@Test
+	void testUpdateTodoSuccess() {
+		var todo = new Todo("todo 1", "desc todo 2", true, 1);
+	
+		webTestClient
+			.put()
+			.uri("/todos")
+			.bodyValue(todo)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$").isArray()
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].nome").isEqualTo(todo.getNome())
+			.jsonPath("$[0].descricao").isEqualTo(todo.getDescricao())
+			.jsonPath("$[0].realizado").isEqualTo(todo.isRealizado())
+			.jsonPath("$[0].prioridade").isEqualTo(todo.getPrioridade());
+	}
+
+	@Test
+	void testUpdateTodoFailure() {
+		webTestClient
+			.put()
+			.uri("/todos/?id=0")
+			.bodyValue(
+				new Todo("todo 2", "desc todo 2", false, 0)
+			)
+			.exchange()
+			.expectStatus().isNotFound();
+	}
+
 
 }
